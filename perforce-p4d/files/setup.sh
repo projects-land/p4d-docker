7b8e8aa2f0ea1b2bfd132a2b08236f05dfc64b4d
Jason Tranchida <jtranchida@gmail.com>
2026-03-03 22:55:38 -0800

    reorder setup

--- perforce-p4d/files/setup.sh
+++ perforce-p4d/files/setup.sh
@@ -17,10 +17,12 @@ if ! p4dctl list 2>/dev/null | grep -q "$NAME"; then
     DEBUG=true MONOCHROME=true /usr/local/bin/configure-p4d.sh "$NAME" -n -p "$P4PORT" -r "$P4ROOT" -u "$P4USER" -P "${P4PASSWD}" --case "$P4CASE" --unicode
 fi
 
+
+
+echo "Starting Perforce Server $NAME..."
+p4dctl start -t p4d "$NAME"
+
 echo p4 configure set $P4NAME#server.depot.root=$P4DEPOTS
 p4 configure set $P4NAME#server.depot.root=$P4DEPOTS
 echo p4 configure set $P4NAME#journalPrefix=$P4CKP/$JNL_PREFIX
 p4 configure set $P4NAME#journalPrefix=$P4CKP/$JNL_PREFIX
-
-echo "Starting Perforce Server $NAME..."
-p4dctl start -t p4d "$NAME"
